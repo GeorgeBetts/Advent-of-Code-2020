b5ee7094c89ca8e6f94e6be7cd7fe896fb5247fb
George Betts <gbetts24@gmail.com>
2020-12-07 23:56:53 +0000

    day 7 complete

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Day 3: :star::star:
 Day 4: :star::star:  
 Day 5: :star::star:  
 Day 6: :star::star:  
+Day 7: :star::star:  
---
+++ day7/day7_part1.php
@@ -0,0 +1,99 @@
+<?php
+$values = file('input.txt', FILE_IGNORE_NEW_LINES);
+
+$bags = [];
+
+foreach ($values as $key => $line) {
+    $explodeLine = explode("contain", $line);
+    $colour = removeBagFromString($explodeLine[0]);
+    $existingBag = getBagByColour($bags, $colour);
+    if ($existingBag) {
+        $existingBag->calculateCanContain($explodeLine[1], $bags);
+    } else {
+        $bag = createBagAndAddToBags($bags, $colour);
+        $bag->calculateCanContain($explodeLine[1], $bags);
+    }
+}
+
+//echo json_encode($bags);
+
+echo countPossibleBags('shiny gold', $bags);
+
+function countPossibleBags($colour, $bags)
+{
+    $count = 0;
+    foreach ($bags as $bag) {
+        if (canBagContain($bag, $colour)) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
+function canBagContain($bag, $colour)
+{
+    foreach ($bag->canContain as $value) {
+        $subBag = $value->bag;
+        if ($subBag->colour == $colour) {
+            return true;
+        } elseif (count($subBag->canContain) > 0) {
+            if (canBagContain($subBag, $colour)) {
+                return true;
+            }
+        }
+    }
+}
+
+class Bag
+{
+
+    public $colour;
+    public $canContain;
+
+    public function __construct($colour)
+    {
+        $this->colour = $colour;
+    }
+
+    public function calculateCanContain($contents, &$bags)
+    {
+        if ($contents != " no other bags.") {
+            $colours = array_map(function ($bag) use (&$bags) {
+                $bagColour = removeBagFromString(substr($bag, 2));
+                $existingBag = getBagByColour($bags, $bagColour);
+                return (object) [
+                    'number' => intval($bag),
+                    'bag' => $existingBag ? $existingBag : createBagAndAddToBags($bags, $bagColour),
+                ];
+            }, explode(",", trim(rtrim($contents, '.'))));
+        } else {
+            $colours = [];
+        }
+        $this->canContain = $colours;
+    }
+
+}
+
+function createBagAndAddToBags(&$bags, $colour)
+{
+    $bag = new Bag($colour);
+    $bags[] = $bag;
+    return $bag;
+}
+
+function removeBagFromString($string)
+{
+    // return rtrim(rtrim(trim($string), ' bags'), 'bag');
+    return preg_replace('/\W\w+\s*(\W*)$/', '$1', trim($string));
+}
+
+//Returns the bag, or returns false if no bag was found
+function getBagByColour($bags, $colour)
+{
+    foreach ($bags as $bag) {
+        if ($bag->colour == $colour) {
+            return $bag;
+        }
+    }
+    return false;
+}
